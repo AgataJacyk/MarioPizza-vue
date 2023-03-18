e03536f3b15ff910cb1a800e2e39a362bff93d03
Agata <73185761+AgataJacyk@users.noreply.github.com>
2023-03-18 22:20:52 +0100

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>  
+    <div id="app">
+    <Header />
+    <div class="box">
+   <router-view></router-view> 
+    </div>
+    <cart/>
+    <Footer /></div> 
+    </template>
+
+<script>
+import Header from '../src/components/layout/Header.vue'
+import Footer from '../src/components/layout/Footer.vue'
+import cart from './components/cart.vue';
+
+export default {
+    components: {
+    Header,
+    Footer,
+    cart
+},
+};
+</script>
+
+
+
